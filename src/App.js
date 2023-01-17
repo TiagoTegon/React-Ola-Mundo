336a6bef17ed41d010765619fb60030dcabaabf1
Tiago Júnio Tegon Nascimento <tiago.tegon@gmail.com>
2023-01-17 10:25:08 -0300

    feat: Start and AboutMe components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AboutMe from "./pages/AboutMe";
+import Start from "./pages/Start";
+
+
 function App() {
   return (
-    <h1>Olá Mundo!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Start />} />
+        <Route path="/aboutme" element={<AboutMe />} />
+        <Route path="*" element={<div>Page not found</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
